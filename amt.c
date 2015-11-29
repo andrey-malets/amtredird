b9c533db88ad7cc46df9a2b2ade762d4cf6efeac
root <root@dijkstra.urgu.org>
2015-11-29 19:51:27 +0500

    Better error handling in amt.c

--- amt.c
+++ amt.c
@@ -9,7 +9,22 @@
 
 #define ERROR_MAX_CHARS 200
 
-void handle_error(IMRResult res) {
+#define GOTO_WITH(label, retval, value) \
+  do { \
+    (retval) = (value); \
+    goto label; \
+  } while (0)
+
+#define IMR_CHECK(call, act) \
+  do { \
+    IMRResult res = call; \
+    if (res != IMR_RES_OK) {\
+      display_error(res); \
+      act; \
+    } \
+  } while (0)
+
+void display_error(IMRResult res) {
   if (res != IMR_RES_OK) {
     char error[ERROR_MAX_CHARS];
     int len;
@@ -37,29 +52,21 @@ int init_amt(struct config *config) {
   assert(config);
   assert(config->amt_ini_filename);
 
-  // Unfortunately, they do not seem to use C constructs properly.
-  char *filename = (char *)config->amt_ini_filename;
-  IMRResult res = IMR_Init(NULL, filename);
-
-  // TODO: get the error message from the bullshit interface.
-  if (res != IMR_RES_OK)
-    goto close;
+  IMR_CHECK(IMR_Init(NULL, (char *)config->amt_ini_filename), goto close);
 
   size_t i;
   for (i = 0; i != config->num_clients; ++i) {
     struct client client = config->clients[i];
     assert(client.host);
-    res = IMR_AddClient(CLI_TCP, (char *)client.host, NULL, &client.id);
-    handle_error(res);
-    if (res != IMR_RES_OK)
-      goto remove_clients;
+    IMR_CHECK(IMR_AddClient(CLI_TCP, (char *)client.host, NULL, &client.id),
+              goto remove_clients);
   }
 
   return 1;
 
 remove_clients:
   for (size_t j = i; j-- != 0;)
-    IMR_RemoveClient(config->clients[j].id);
+    IMR_CHECK(IMR_RemoveClient(config->clients[j].id), ;);
 
 close:
   IMR_Close();
@@ -69,8 +76,8 @@ close:
 void teardown_amt(const struct config *config) {
   assert(config);
   for (size_t i = config->num_clients; i-- != 0;)
-    IMR_RemoveClient(config->clients[i].id);
-  IMR_Close();
+    IMR_CHECK(IMR_RemoveClient(config->clients[i].id), ;);
+  IMR_CHECK(IMR_Close(), ;);
 }
 
 int start_client(const struct config *config, const struct client *client) {
@@ -82,14 +89,45 @@ int start_client(const struct config *config, const struct client *client) {
   strncpy(params.user_name, get_username(config, client), MAX_NAME_LEN);
   strncpy(params.user_pswd, get_passwd(config, client), MAX_PSWD_LEN);
 
-  IMRResult res = IMR_IDEROpenTCPSession(
-      client->id, &params, NULL, (char *)client->filename, (char *)EMPTY_DEV);
-  handle_error(res);
+  IMR_CHECK(
+      IMR_IDEROpenTCPSession(client->id, &params, NULL,
+                             (char *)client->filename, (char *)EMPTY_DEV),
+      goto ret);
 
-  return res == IMR_RES_OK;
+  IDERDeviceCmd cmd = {.pri_op = IDER_ENABLE, .pri_timing = IDER_SET_ONRESET};
+  IDERDeviceResult result;
+  IMR_CHECK(IMR_IDERSetDeviceState(client->id, &cmd, &result), goto free);
+
+  if (result.pri_res != IDER_DONE) {
+    fputs("IDERDeviceResult is not IDER_DONE\n", stderr);
+    goto free;
+  }
+
+  return 1;
+
+free:
+  IMR_CHECK(IMR_IDERCloseSession(client->id), ;);
+ret:
+  return 0;
 }
 
 int stop_client(const struct client *client) {
   assert(client);
-  return IMR_IDERCloseSession(client->id) == IMR_RES_OK;
+  int rv = 1;
+
+  IDERDeviceCmd cmd = {.pri_op = IDER_DISABLE,
+                       .pri_timing = IDER_SET_IMMEDIATELY};
+  IDERDeviceResult result;
+  IMR_CHECK(IMR_IDERSetDeviceState(client->id, &cmd, &result),
+            GOTO_WITH(free, rv, 0));
+
+  if (result.pri_res != IDER_DONE) {
+    fputs("warning: IDERDeviceResult is not IDER_DONE\n", stderr);
+    rv = 0;
+  }
+
+free:
+  IMR_CHECK(IMR_IDERCloseSession(client->id), GOTO_WITH(ret, rv, 0));
+ret:
+  return rv;
 }
